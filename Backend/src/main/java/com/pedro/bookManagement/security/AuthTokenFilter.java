e2c2ad1333530c2549b52696e9cc1806cdf07c36
pedroheringerr <pedroheringerrc@gmail.com>
2025-12-22 22:57:58 -0300

    feat: add Authentication with JWT tokens setup

---
+++ Backend/src/main/java/com/pedro/bookManagement/security/AuthTokenFilter.java
@@ -0,0 +1,70 @@
+package com.pedro.bookManagement.security;
+
+import java.io.IOException;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.pedro.bookManagement.service.CustomUserDetailsService;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class AuthTokenFilter extends OncePerRequestFilter {
+
+	private JwtUtil jwtUtils;
+
+	private CustomUserDetailsService userDetailsService;
+
+	public AuthTokenFilter(JwtUtil jwtUtils, CustomUserDetailsService userDetailsService) {
+		this.jwtUtils = jwtUtils;
+		this.userDetailsService = userDetailsService;
+	}
+
+	@Override
+	protected void doFilterInternal(
+			HttpServletRequest request,
+			HttpServletResponse response,
+			FilterChain filterChain
+	) throws ServletException, IOException {
+			try {
+				String jwt = parseJwt(request);
+        if (jwt != null && jwtUtils.validateJwtToken(jwt)) {
+            String username = jwtUtils.getUsernameFromToken(jwt);
+            UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+            UsernamePasswordAuthenticationToken authentication =
+                    new UsernamePasswordAuthenticationToken(
+                            userDetails,
+                            null,
+                            userDetails.getAuthorities()
+                    );
+						System.out.println("JWT valid: " + jwtUtils.validateJwtToken(jwt));
+            authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+						System.out.println("Authenticated user: " +
+					    SecurityContextHolder.getContext().getAuthentication());
+        }
+      } catch (Exception e) {
+            System.out.println("Cannot set user authentication: " + e);
+        }
+			System.out.println("Authorization header: " + request.getHeader("Authorization"));
+      filterChain.doFilter(request, response);
+
+}
+
+  private String parseJwt(HttpServletRequest request) {
+      String headerAuth = request.getHeader("Authorization");
+      if (headerAuth != null && headerAuth.startsWith("Bearer ")) {
+          return headerAuth.substring(7);
+      }
+      return null;
+	}
+	
+}

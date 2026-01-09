64dda1b98313ff17ef825709d339e8c740eaaf9c
pedroheringerr <pedroheringerrc@gmail.com>
2026-01-09 18:01:49 -0300

    chore: configure routing and providers

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,10 +1,50 @@
 import { Routes } from '@angular/router';
-import { Home } from './pages/home/home';
+import { authGuard } from './guard/auth-guard';
+import { roleGuard } from './guard/role-guard';
 
 export const routes: Routes = [
   {
     path: '',
     pathMatch: 'full',
-    component: Home
+    loadComponent: () =>
+      import('./pages/home/home').then(m => m.Home)
+  },
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./pages/login/login').then(m => m.Login)
+  },
+  {
+    path: 'signup',
+    loadComponent: () =>
+      import('./pages/signup/signup').then(m => m.Signup)
+  },
+  {
+    path: 'books/edit/:isbn',
+    loadComponent: () =>
+      import('./pages/edit-book/edit-book').then(m => m.EditBook),
+    canActivate: [authGuard, roleGuard],
+    data: { roles: [ 'ADMIN', 'USER' ] }
+  },
+  {
+    path:'books/add',
+    loadComponent: () =>
+      import('./pages/edit-book/edit-book').then(m => m.EditBook),
+    canActivate: [authGuard, roleGuard],
+    data: { roles: [ 'ADMIN', 'USER' ] }
+  },
+  {
+    path:'users',
+    loadComponent: () =>
+      import('./pages/user/user').then(m => m.User),
+    canActivate: [authGuard, roleGuard],
+    data: { roles: [ 'ADMIN' ] }
+  },
+  {
+    path: 'users/edit/:id',
+    loadComponent: () =>
+      import('./pages/edit-user/edit-user').then(m => m.EditUser),
+    canActivate: [authGuard, roleGuard],
+    data: { roles: [ 'ADMIN' ] }
   }
 ];

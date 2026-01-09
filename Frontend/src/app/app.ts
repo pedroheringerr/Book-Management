54c78fcf76658f800a4749b59e03c445643c8d15
pedroheringerr <pedroheringerrc@gmail.com>
2026-01-09 18:03:17 -0300

    feat: add global styles with bootstrap

--- Frontend/src/app/app.ts
+++ Frontend/src/app/app.ts
@@ -1,12 +1,18 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './header/header';
+import { ConfirmModal } from './shared/confirm-modal/confirm-modal';
+import { Theme } from './theme/theme';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, ConfirmModal],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('Frontend');
+
+  themeService = inject(Theme);
+
+  constructor () {}
 }

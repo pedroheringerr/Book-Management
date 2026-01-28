5b4e890695854972cf10018da5b77a8c4f0e591b
pedroheringerr <pedroheringerrc@gmail.com>
2026-01-28 10:14:14 -0300

    fix: frontend connection with backend with wrong url

--- Backend/src/main/java/com/pedro/bookManagement/config/WebSecurityConfiguration.java
+++ Backend/src/main/java/com/pedro/bookManagement/config/WebSecurityConfiguration.java
@@ -1,7 +1,9 @@
 package com.pedro.bookManagement.config;
 
+import com.pedro.bookManagement.security.AuthEntryPointJwt;
+import com.pedro.bookManagement.security.AuthTokenFilter;
+import com.pedro.bookManagement.service.CustomUserDetailsService;
 import java.util.List;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -20,83 +22,79 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import com.pedro.bookManagement.security.AuthEntryPointJwt;
-import com.pedro.bookManagement.security.AuthTokenFilter;
-import com.pedro.bookManagement.security.JwtUtil;
-import com.pedro.bookManagement.service.CustomUserDetailsService;
-
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class WebSecurityConfiguration implements WebMvcConfigurer {
 
-    private final CustomUserDetailsService userDetailsService;
+  private final CustomUserDetailsService userDetailsService;
 
-    private final AuthEntryPointJwt unauthorizedHandler;
+  private final AuthEntryPointJwt unauthorizedHandler;
 
-		private final AuthTokenFilter authTokenFilter;
+  private final AuthTokenFilter authTokenFilter;
 
-		public WebSecurityConfiguration(
-        CustomUserDetailsService userDetailsService,
-        AuthEntryPointJwt unauthorizedHandler,
-				AuthTokenFilter authTokenFilter
-    ) {
-        this.userDetailsService = userDetailsService;
-        this.unauthorizedHandler = unauthorizedHandler;
-				this.authTokenFilter = authTokenFilter;
-    }
+  public WebSecurityConfiguration(
+      CustomUserDetailsService userDetailsService,
+      AuthEntryPointJwt unauthorizedHandler,
+      AuthTokenFilter authTokenFilter) {
+    this.userDetailsService = userDetailsService;
+    this.unauthorizedHandler = unauthorizedHandler;
+    this.authTokenFilter = authTokenFilter;
+  }
 
-		@Override
-		public void addCorsMappings(CorsRegistry registry) {
-			registry.addMapping("/**")
-            .allowedOrigins("http://localhost:4200")
-            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
-		}
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry
+        .addMapping("/**")
+        .allowedOrigins("http://localhost:4200")
+        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+  }
 
-		@Bean
-		public CorsConfigurationSource corsConfigurationSource() {
-			CorsConfiguration config = new CorsConfiguration();
+  @Bean
+  public CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration config = new CorsConfiguration();
 
-			config.setAllowedOrigins(List.of("http://localhost:4200"));
-			config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-			config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
-			config.setExposedHeaders(List.of("Authorization"));
-			config.setAllowCredentials(true);
+    config.setAllowedOrigins(List.of("http://localhost:4200"));
+    config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+    config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
+    config.setExposedHeaders(List.of("Authorization"));
+    config.setAllowCredentials(true);
 
-			UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-			source.registerCorsConfiguration("/**", config);
-			return source;
-		}
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", config);
+    return source;
+  }
 
-
-    @Bean
-    public AuthenticationManager authenticationManager(
-            AuthenticationConfiguration authenticationConfiguration
-    ) throws Exception {
-        return authenticationConfiguration.getAuthenticationManager();
-    }
+  @Bean
+  public AuthenticationManager authenticationManager(
+      AuthenticationConfiguration authenticationConfiguration) throws Exception {
+    return authenticationConfiguration.getAuthenticationManager();
+  }
 
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-    http
-							.cors(Customizer.withDefaults())
-              .csrf(csrf -> csrf.disable()) // Disable CSRF
-              .exceptionHandling(exceptionHandling ->
-                      exceptionHandling.authenticationEntryPoint(unauthorizedHandler)
-              )
-              .sessionManagement(sessionManagement ->
-                      sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-              )
-              .authorizeHttpRequests(authorizeRequests ->
-                      authorizeRequests
-															.requestMatchers("/api/auth/**").permitAll()
-															.requestMatchers("/api/books/stats/**").permitAll()
-															.requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
-															.requestMatchers(HttpMethod.GET, "/api/books/**").permitAll()
-                              .anyRequest().authenticated()
-              );
-      // Add the JWT Token filter before the UsernamePasswordAuthenticationFilter
-      http.addFilterBefore(authTokenFilter, UsernamePasswordAuthenticationFilter.class);
-      return http.build();
+    http.cors(Customizer.withDefaults())
+        .csrf(csrf -> csrf.disable()) // Disable CSRF
+        .exceptionHandling(
+            exceptionHandling -> exceptionHandling.authenticationEntryPoint(unauthorizedHandler))
+        .sessionManagement(
+            sessionManagement ->
+                sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .authorizeHttpRequests(
+            authorizeRequests ->
+                authorizeRequests
+                    .requestMatchers("/api/auth/**")
+                    .permitAll()
+                    .requestMatchers("/api/books/stats/**")
+                    .permitAll()
+                    .requestMatchers(HttpMethod.OPTIONS, "/**")
+                    .permitAll()
+                    .requestMatchers(HttpMethod.GET, "/api/books/**")
+                    .permitAll()
+                    .anyRequest()
+                    .authenticated());
+    // Add the JWT Token filter before the UsernamePasswordAuthenticationFilter
+    http.addFilterBefore(authTokenFilter, UsernamePasswordAuthenticationFilter.class);
+    return http.build();
   }
 }
--- Backend/src/main/java/com/pedro/bookManagement/security/AuthTokenFilter.java
+++ Backend/src/main/java/com/pedro/bookManagement/security/AuthTokenFilter.java
@@ -1,7 +1,11 @@
 package com.pedro.bookManagement.security;
 
+import com.pedro.bookManagement.service.CustomUserDetailsService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
-
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -9,74 +13,54 @@ import org.springframework.security.web.authentication.WebAuthenticationDetailsS
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import com.pedro.bookManagement.service.CustomUserDetailsService;
-
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-
 @Component
 public class AuthTokenFilter extends OncePerRequestFilter {
 
-	private JwtUtil jwtUtils;
+  private JwtUtil jwtUtil;
 
-	private CustomUserDetailsService userDetailsService;
+  private CustomUserDetailsService userDetailsService;
 
-	public AuthTokenFilter(JwtUtil jwtUtils, CustomUserDetailsService userDetailsService) {
-		this.jwtUtils = jwtUtils;
-		this.userDetailsService = userDetailsService;
-	}
+  public AuthTokenFilter(JwtUtil jwtUtil, CustomUserDetailsService userDetailsService) {
+    this.jwtUtil = jwtUtil;
+    this.userDetailsService = userDetailsService;
+  }
 
-	@Override
-	protected void doFilterInternal(
-			HttpServletRequest request,
-			HttpServletResponse response,
-			FilterChain filterChain
-	) throws ServletException, IOException {
-			try {
-				String jwt = parseJwt(request);
-        if (jwt != null) {
-            if (jwtUtils.validateJwtToken(jwt)) {
-                String username = jwtUtils.getUsernameFromToken(jwt);
+  @Override
+  protected void doFilterInternal(
+      HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+      throws ServletException, IOException {
+    try {
+      String jwt = parseJwt(request);
+      if (jwt != null) {
+        if (jwtUtil.validateJwtToken(jwt)) {
+          String username = jwtUtil.getUsernameFromToken(jwt);
 
-                UserDetails userDetails =
-                        userDetailsService.loadUserByUsername(username);
+          UserDetails userDetails = userDetailsService.loadUserByUsername(username);
 
-                UsernamePasswordAuthenticationToken authentication =
-                        new UsernamePasswordAuthenticationToken(
-                                userDetails,
-                                null,
-                                userDetails.getAuthorities()
-                        );
+          UsernamePasswordAuthenticationToken authentication =
+              new UsernamePasswordAuthenticationToken(
+                  userDetails, null, userDetails.getAuthorities());
 
-                authentication.setDetails(
-                        new WebAuthenticationDetailsSource().buildDetails(request)
-                );
+          authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
 
-                SecurityContextHolder.getContext()
-                        .setAuthentication(authentication);
-						}
-						else {
-							SecurityContextHolder.clearContext();
-						}
-					}
-				}
-				catch (Exception e) {
-					SecurityContextHolder.clearContext();
-					System.out.println("JWT processing failed: " + e.getMessage());
+          SecurityContextHolder.getContext().setAuthentication(authentication);
+        } else {
+          SecurityContextHolder.clearContext();
         }
-			System.out.println("Authorization header: " + request.getHeader("Authorization"));
-      filterChain.doFilter(request, response);
-
-}
+      }
+    } catch (Exception e) {
+      SecurityContextHolder.clearContext();
+      System.out.println("JWT processing failed: " + e.getMessage());
+    }
+    System.out.println("Authorization header: " + request.getHeader("Authorization"));
+    filterChain.doFilter(request, response);
+  }
 
   private String parseJwt(HttpServletRequest request) {
-      String headerAuth = request.getHeader("Authorization");
-      if (headerAuth != null && headerAuth.startsWith("Bearer ")) {
-          return headerAuth.substring(7);
-      }
-      return null;
-	}
-	
+    String headerAuth = request.getHeader("Authorization");
+    if (headerAuth != null && headerAuth.startsWith("Bearer ")) {
+      return headerAuth.substring(7);
+    }
+    return null;
+  }
 }
